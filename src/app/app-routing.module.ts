d70236a7b09c5a587b9fbbcd17b7febaf8e71523
Aman Singh Parihar <amanparihar16@gmail.com>
2024-02-20 18:49:51 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'seller-auth',
+    component: SellerAuthComponent
+  },
+  {
+    path: 'seller-home',
+    component: SellerHomeComponent,
+    canActivate: [authGuard]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
